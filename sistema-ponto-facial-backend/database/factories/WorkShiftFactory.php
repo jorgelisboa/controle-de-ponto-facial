5e96f158c0a97f288c9bd8a8bfd19321336db582
krhuhun <jorgelisboa659@gmail.com>
2024-09-25 23:23:33 -0300

    feat:  Workshift seeders maden

--- sistema-ponto-facial-backend/database/factories/WorkShiftFactory.php
+++ sistema-ponto-facial-backend/database/factories/WorkShiftFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Collaborator;
+use App\Models\WorkShift;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +12,29 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class WorkShiftFactory extends Factory
 {
+
+    protected $model = WorkShift::class;
+
     /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition(): array
+    public function definition()
     {
+        // Pegue um colaborador existente
+        $collaborator = Collaborator::inRandomOrder()->first();
+
+        // Defina um dia aleatório para o ponto de trabalho
+        $startTime = Carbon::parse(time: $this->faker->dateTimeBetween('-2 month', 'now'));
+
+        // Horário de entrada
+        $endTime = (clone $startTime)->addHours(rand(4, 12)); // Entre 8 e 12 horas de trabalho
+
         return [
-            //
+            'collaborator_document' => $collaborator->document,
+            'created_at' => $startTime, // Timestamp de entrada
+            'updated_at' => $endTime, // Timestamp de saída
         ];
     }
 }
