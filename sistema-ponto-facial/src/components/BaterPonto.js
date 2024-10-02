d63ad6a450d3597f0cf887b9f8996a40374000e1
Vitor Justamand <justamandd@gmail.com>
2024-10-02 11:57:41 -0300

    feat(ponto): ponto funcionando com url estática no .env

--- sistema-ponto-facial/src/components/BaterPonto.js
+++ sistema-ponto-facial/src/components/BaterPonto.js
@@ -1,14 +1,31 @@
 import { StyleSheet } from "react-native";
 import { FAB } from "react-native-paper";
+// import { API_URL } from "react-native-dotenv";
 
 export default function BaterPonto() {
+  const baterPonto = () => {
+    console.log(process.env.EXPO_PUBLIC_API_URL + "/shifts");
+    fetch(process.env.API_URL + "/shifts", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "ngrok-skip-browser-warning": undefined,
+      },
+      body: JSON.stringify({
+        collaborator_document: "11111111111",
+      }),
+    }).catch((e) => {
+      console.log(e.message);
+    });
+  };
   return (
     <FAB
       icon="plus"
       label="Bater ponto"
       accessibilityLabel="Bater ponto"
       style={styles.fab}
-      onPress={() => console.log("Pressed")}
+      onPress={baterPonto}
       variant="surface"
     />
   );
