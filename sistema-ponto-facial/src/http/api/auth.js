08c6e8036df53cfc1c8bb8e5af81a1930dfb0db2
rafael-azevedo-select <rafael.azevedo@selectsolucoes.com>
2024-11-26 01:18:36 -0300

    polindo o register

--- sistema-ponto-facial/src/http/api/auth.js
+++ sistema-ponto-facial/src/http/api/auth.js
@@ -14,38 +14,42 @@ import { localhost } from ".";
  * @returns
  */
 export async function register(user) {
-  // Send request with user data to register endpoint (make with form data because of the file)
+  // Criação do FormData
   const formData = new FormData();
   formData.append("email", user.email);
   formData.append("password", user.password);
-  formData.append("name", user.full_name);
+  formData.append("name", user.name); // Alterado para name como está na interface
   formData.append("document", user.document);
   formData.append("role", user.role);
   formData.append("hourly_value", user.hourly_value);
   formData.append("estimated_journey", user.estimated_journey);
+
+  // Supondo que o profile_photo_path seja um arquivo
   formData.append("profile_photo", user.profile_photo_path);
 
-  const response = await fetch(`${localhost}/register`, {
-    method: "POST",
-    body: formData,
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "multipart/form-data",
-      "ngrok-skip-browser-warning": "true",
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
+  try {
+    // Configurando a URL e opções da requisição
+    const response = await fetch("http://98.84.198.179/api/register", {
+      method: "POST",
+      body: formData,
+      headers: {
+        "Accept": "application/json",
+        "ngrok-skip-browser-warning": "true",
+      },
+    });
 
-  if (response.ok) {
-    return await response.json();
-  } else {
-    const errorData = await response.json();
-    console.log(response);
-    console.error(`Error ${response.status}: ${errorData.message}`);
-    return errorData;
+    // Tratando a resposta
+    if (response.ok) {
+      return await response.json();
+    } else {
+      const errorData = await response.json();
+      console.error(`Erro ${response.status}: ${errorData.message}`);
+      return errorData;
+    }
+  } catch (error) {
+    console.error("Erro ao fazer a requisição:", error);
+    return null;
   }
-
-  return null;
 }
 
 export async function login({ email, password }) {
